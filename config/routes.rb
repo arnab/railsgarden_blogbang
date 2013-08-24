0c7d6de7810c81658a590723fc3b854dc604e1a7
Arnab Deka <arnab.deka@gmail.com>
2013-08-24 17:00:18 +0530

    comments flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Blogbang::Application.routes.draw do
-  resources :posts
+  get "comments/create"
+  resources :posts do
+    resources :comments, only: :create
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130824073320) do
+ActiveRecord::Schema.define(version: 20130824104949) do
+
+  create_table "comments", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "post_id"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
